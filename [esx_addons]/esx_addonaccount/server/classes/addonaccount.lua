3798f9005f9eb92e24c28acba248057bc7e0de98
Mirow <maxim.doerr@icloud.com>
2025-09-04 20:05:56 +0200

    refactor(addonaccount): add type annotations
    Added type annotations and refactored some functions.

--- [esx_addons]/esx_addonaccount/server/classes/addonaccount.lua
+++ [esx_addons]/esx_addonaccount/server/classes/addonaccount.lua
@@ -1,35 +1,54 @@
-function CreateAddonAccount(name, owner, money)
-	local self = {}
+---@class AddonAccount
+---@field name string
+---@field owner? string
+---@field money number
+---@field addMoney fun(amount: number)
+---@field removeMoney fun(amount: number)
+---@field setMoney fun(amount: number)
+---@field save function
 
-	self.name  = name
+---@param name string
+---@param owner? string
+---@param money number
+---@return AddonAccount
+function CreateAddonAccount(name, owner, money)
+	---@diagnostic disable-next-line: missing-fields
+	local self = {} --[[@type AddonAccount]]
+
+	self.name = name
 	self.owner = owner
 	self.money = money
 
 	function self.addMoney(amount)
-		self.money = self.money + amount
+		self.money += amount
 		self.save()
+
 		TriggerEvent('esx_addonaccount:addMoney', self.name, amount)
 	end
 
 	function self.removeMoney(amount)
-		self.money = self.money - amount
+		self.money -= amount
 		self.save()
+
 		TriggerEvent('esx_addonaccount:removeMoney', self.name, amount)
 	end
 
 	function self.setMoney(amount)
 		self.money = amount
 		self.save()
+
 		TriggerEvent('esx_addonaccount:setMoney', self.name, amount)
 	end
 
 	function self.save()
-		if self.owner == nil then
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ?', { self.money, self.name })
-		else
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ? AND owner = ?',
-				{ self.money, self.name, self.owner })
+		local query, params = 'UPDATE addon_account_data SET money = ? WHERE account_name = ?', { self.money, self.name }
+
+		if self.owner then
+			query = query .. ' AND owner = ?'
+			table.insert(params, self.owner)
 		end
+
+		MySQL.update.await(query, params)
 		TriggerClientEvent('esx_addonaccount:setMoney', -1, self.name, self.money)
 	end
 
