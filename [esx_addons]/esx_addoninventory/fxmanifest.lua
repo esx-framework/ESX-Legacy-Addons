8797ceb7816c2b5afed15e5a17a8e91b70fae10e
Mirow <maxim.doerr@icloud.com>
2025-09-06 20:18:00 +0200

    chore(esx_addoninventory): update manifest

--- [esx_addons]/esx_addoninventory/fxmanifest.lua
+++ [esx_addons]/esx_addoninventory/fxmanifest.lua
@@ -1,23 +1,22 @@
-fx_version 'adamant'
-
+fx_version 'cerulean'
 game 'gta5'
-
-description 'Adds a way for resources to store items for players'
 lua54 'yes'
+use_fxv2_oal 'yes'
 
-version '1.0'
+author 'ESX-Framework'
+description 'Adds a way for resources to store items for players.'
+version '1.1'
 legacyversion '1.13.4'
 
 server_scripts {
 	'@es_extended/imports.lua',
 	'@oxmysql/lib/MySQL.lua',
 	'server/classes/addoninventory.lua',
+	'server/database.lua',
 	'server/main.lua'
 }
 
-server_exports {
-	'GetSharedInventory',
-	'AddSharedInventory'
-}
-
-dependency 'es_extended'
+-- server_exports {
+-- 	'GetSharedInventory',
+-- 	'AddSharedInventory'
+-- }
