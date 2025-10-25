9b88cc7d4c25fa958e7fd316744ac8eb435d555c
Kenshin13 <63159154+Kenshiin13@users.noreply.github.com>
2025-10-25 17:40:37 +0200

    refactor: add cfg type annotations

--- [esx_addons]/esx_weather/config/main.lua
+++ [esx_addons]/esx_weather/config/main.lua
@@ -1,37 +1,51 @@
-Config = {}
-Config.Weather = {}
-Config.Time = {}
+---@class WeatherConfig
+---@field ValidTypes WeatherType[]
+---@field transitionTimeSeconds integer
+---@field cycleTimeSeconds integer
 
-Config.debug = true
+---@class TimeConfig
+---@field secondsPerGameMinute integer
+---@field Zones table<string, integer>
 
-Config.AdminGroups = {
-    admin = true,
-    moderator = true,
-}
+---@class Config
+---@field debug boolean
+---@field AdminGroups table<string, boolean>
+---@field Zones table<string, vector2>
+---@field Weather WeatherConfig
+---@field Time TimeConfig
 
-Config.Zones = {
-    ["Los Santos"] = vector2(-33.6997, -1102.0000),
-    ["Sandy Shores"] = vector2(1728.2000, 3709.3000),
-    ["Paleto Bay"] = vector2(-220.2753, 6408.9116),
-}
+Config = {
+    debug       = true,
+    AdminGroups = {
+        admin = true,
+        moderator = true,
+    },
+    Zones       = {
+        ["Los Santos"] = vector2(-33.6997, -1102.0000),
+        ["Sandy Shores"] = vector2(1728.2000, 3709.3000),
+        ["Paleto Bay"] = vector2(-220.2753, 6408.9116),
+    },
 
-Config.Weather.ValidTypes = {
-    Shared.Enum.WeatherType.CLEAR,
-    Shared.Enum.WeatherType.CLOUDS,
-    Shared.Enum.WeatherType.EXTRASUNNY,
-    Shared.Enum.WeatherType.FOGGY,
-    Shared.Enum.WeatherType.OVERCAST,
-    Shared.Enum.WeatherType.RAIN,
-    Shared.Enum.WeatherType.SMOG,
-    Shared.Enum.WeatherType.THUNDER,
-}
-
-Config.Weather.transitionTimeSeconds = 30
-Config.Weather.cycleTimeSeconds = 60 * 30 -- 30 minutes
-
-Config.Time.secondsPerGameMinute = 15
-Config.Time.Zones = {
-    ["Los Santos"] = 0,
-    ["Sandy Shores"] = 5,
-    ["Paleto Bay"] = 3,
-}
+    Weather     = {
+        ValidTypes = {
+            Shared.Enum.WeatherType.CLEAR,
+            Shared.Enum.WeatherType.CLOUDS,
+            Shared.Enum.WeatherType.EXTRASUNNY,
+            Shared.Enum.WeatherType.FOGGY,
+            Shared.Enum.WeatherType.OVERCAST,
+            Shared.Enum.WeatherType.RAIN,
+            Shared.Enum.WeatherType.SMOG,
+            Shared.Enum.WeatherType.THUNDER,
+        },
+        transitionTimeSeconds = 30,
+        cycleTimeSeconds = 60 * 30 -- 30 minutes
+    },
+    Time        = {
+        secondsPerGameMinute = 15,
+        Zones = {
+            ["Los Santos"] = 0,
+            ["Sandy Shores"] = 5,
+            ["Paleto Bay"] = 3,
+        }
+    }
+} --[[@as Config]]
