815d4c107a141b54c59f79147a3a46faa00c6518
Kenshin13 <63159154+Kenshiin13@users.noreply.github.com>
2025-10-25 17:34:29 +0200

    refactor: avoid useless obj creation

--- [esx_addons]/esx_weather/server/modules/time/main.lua
+++ [esx_addons]/esx_weather/server/modules/time/main.lua
@@ -3,18 +3,16 @@ Modules.Time = {}
 
 ---@param src integer?
 function Modules.Time.broadcast(src)
-    TriggerClientEvent("esx_weather:client:time:setTime", (src or -1), Modules.Time.get():serialize())
+    TriggerClientEvent("esx_weather:client:time:setTime", (src or -1), Modules.Time.get())
 end
 
----@return Time
+---@return SerializedTime
 function Modules.Time.get()
     local t = os.date("*t")
 
-    local SerializedTime = {
+    return {
         hours = tonumber(t.hour) or 0,
         minutes = tonumber(t.min) or 0,
         seconds = tonumber(t.sec) or 0
     } --[[@as SerializedTime]]
-
-    return Shared.Class.Time:new(SerializedTime)
 end
